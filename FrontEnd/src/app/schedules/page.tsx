17c70341ae59b0d3b8ea9a0db037353674b3e56f
sadmorello <dmurild-juvencio@hotmail.com.br>
2024-07-15 21:06:12 -0300

    Agendamentos Alterado Layout para utilizar Cards

--- FrontEnd/src/app/schedules/page.tsx
+++ FrontEnd/src/app/schedules/page.tsx
@@ -1,36 +1,31 @@
 import { Button, ButtonGroup } from "@chakra-ui/button";
-import { Box, Center, Container, Flex, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { Text, Card, CardBody, CardFooter, Container, Flex, Heading, Stack, Box, Spacer } from "@chakra-ui/react";
 import React from "react";
 
 export default function Schedules() {
     return (
-        <Container>
-            <Flex justify='center' p={5}>
-                <ButtonGroup>
-                    <Button colorScheme='green'>Incluir</Button>
-                    <Button colorScheme='blue'>Visualizar</Button>
-                    <Button colorScheme='red'>Cancelar</Button>
-                </ButtonGroup>
+        <>
+            <Flex p={5}>
+                <Heading>Agendamentos</Heading>
+                <Spacer />
+                <Button colorScheme='green' size='lg'>Novo Agendamento</Button>
             </Flex>
 
-            <TableContainer>
-                <Table variant='simple'>                    
-                    <Thead>
-                        <Tr>
-                            <Th>Numero</Th>
-                            <Th>Piscologo(a)</Th>
-                            <Th>Data</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td>1</Td>
-                            <Td>Sabrina</Td>
-                            <Td>15/07/2024</Td>
-                        </Tr>                        
-                    </Tbody>                    
-                </Table>
-            </TableContainer>
-        </Container>
+            <Card variant='outline'>
+                <Stack>
+                    <CardBody>
+                        <Heading size='md'>Agendamento com Sabrina</Heading>
+                    </CardBody>
+
+                    <CardFooter>
+                        <Text fontSize='lg'> 15/07/2024</Text>
+                        <Spacer />
+                        <ButtonGroup>
+                            <Button colorScheme='red' size='lg'>Cancelar</Button>
+                        </ButtonGroup>
+                    </CardFooter>
+                </Stack>
+            </Card>
+        </>
     );
 }
