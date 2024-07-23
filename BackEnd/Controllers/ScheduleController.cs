ed3ee35f7a5ac1cc276694dd207448887ba2c36b
sadmorello <dmurild-juvencio@hotmail.com.br>
2024-07-23 18:41:15 -0300

    Persistindo registros no banco

--- BackEnd/Controllers/ScheduleController.cs
+++ BackEnd/Controllers/ScheduleController.cs
@@ -1,3 +1,4 @@
+using BackEnd.Data;
 using BackEnd.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,29 +8,35 @@ namespace BackEnd.Controllers;
 [Route("[controller]")]
 public class ScheduleController : ControllerBase
 {
-    private static List<Schedule> schedueles = new List<Schedule>();
+    private SchedulingContext _context;
+
+    public ScheduleController(SchedulingContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult CreateSchedule([FromBody] Schedule scheduling)
     {
-        schedueles.Add(scheduling);
+        _context.Schedules.Add(scheduling);
+        _context.SaveChanges();
 
         return CreatedAtAction(nameof(GetScheduleById), new { id = scheduling.Id }, scheduling);
     }
 
     [HttpGet]
-    public IEnumerable<Schedule> GetSchedule()
+    public IEnumerable<Schedule> GetSchedule([FromBody] int skip = 0, [FromBody] int take = 5)
     {
-        return schedueles;
+        return _context.Schedules.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetScheduleById(int id)
     {
-        Schedule? scheduling = schedueles.FirstOrDefault(scheduling => scheduling.Id == id);
+        Schedule? scheduling = _context.Schedules.FirstOrDefault(scheduling => scheduling.Id == id);
 
         if (scheduling == null) return NotFound();
-        
+
         return Ok(scheduling);
     }
 
